b6712c6723e2cdde3c03b1bdc9df227c5ce7da56
nandiniiee <jain.nandini.squash@gmail.com>
2026-05-20 08:12:38 +0000

    Draft 1 bash script for netplan apaply

--- netplanApply.sh
+++ netplanApply.sh
@@ -3,26 +3,26 @@
 # Checking if it is accessed using root directory or not 
 if [[ $EUID -ne 0 ]]; then
 	whiptail --msgbox "Permission denied" 10 40
-	whiptail --msgbox "Script must be run as root. Re-run the script using sudo command"
+	whiptail --msgbox "Script must be run as root. Re-run the script using sudo command" 10 40
 	exit 1
 fi
 
 # Interface
-INTERFACE=$(whiptail --inputbox "Please enter the name of your interface" 10 40 3>$1 1>$2 2>$3)
+INTERFACE=$(whiptail --inputbox "Please enter the name of your interface" 10 40 3>&1 1>&2 2>&3)
 
 # IP
-IP=$(whiptail --inputbox "Please enter your static IP address" 10 40 3>$1 1>$2 2>$3)
+IP=$(whiptail --inputbox "Please enter your static IP address" 10 40 3>&1 1>&2 2>&3)
 
 # CIDR
-CDIR=$(whiptail --inputbox "Please enter CIDRk" 10 40 3>$1 1>$2 2>$3)
+CIDR=$(whiptail --inputbox "Please enter CIDR" 10 40 3>&1 1>&2 2>&3)
 
 # gateway
-GATEWAY=$(whiptail --inputbox "Please enter your gateway IP" 10 40 3>$1 1>$2 2>$3)
+GATEWAY=$(whiptail --inputbox "Please enter your gateway IP" 10 40 3>&1 1>&2 2>&3)
 
 # DNS Server
-DNS=$(whiptail --inputbox "Please enter your DNS Server" 10 40 3>$1 1>$2 2>$3) 
+DNS=$(whiptail --inputbox "Please enter your DNS Server" 10 40 3>&1 1>&2 2>&3) 
 
-# validating ip using a fucntion
+# validating ips using a fucntion
 validateIP(){
 	if [[ $1 =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]];then
 		return 0
@@ -30,5 +30,52 @@ validateIP(){
 		return 1
 	fi
 }
+
+# validate ip
+if ! validateIP "$IP"; then
+	whiptail --msgbox "Invalid IP address" 10 40
+	exit 1
+fi
+
+# validate gateway
+if ! validateIP "$GATEWAY"; then
+	whiptail --msgbox "Invalid gateway address" 10 40
+	exit 1
+fi
+
+# validate DNS
+if ! validateIP "$DNS"; then
+	whiptail --msgbox "Invalid DNS server"; 10 40
+	exit 1
+fi
+
+# creating netplan configuration file
+cat <<EOF > /etc/netplan/01-static.yaml
+network:
+ version: 2
+ renderer: networkd
+
+ ethernets:
+  $INTERFACE:
+   dhcp: no
+   addresses: 
+    - $IP/$CIDR
+
+   routes:
+    - to: default
+    via: $GATEWAY
+
+   nameservers:
+   addresses:
+    - $DNS
+EOF
+
+# Applying netplan configuration
+netplan apply
+
+#success message
+whiptail --msgbox "Successfully configured" 10 52
+
+
 
 
