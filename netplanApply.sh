899d6a36fd42ced3f10e5563d9f9a03fcb59ff79
nandiniiee <jain.nandini.squash@gmail.com>
2026-05-20 10:42:28 +0000

    final one with frop down menu for network interfaces

--- netplanApply.sh
+++ netplanApply.sh
@@ -8,23 +8,56 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # Interface
-INTERFACE=$(whiptail --inputbox "Please enter the name of your interface" 10 40 3>&1 1>&2 2>&3)
+ALL_INTERFACES=$(ip -o link show)
+INTERFACE_NAMES=$(echo "$ALL_INTERFACES" | awk -F': ' '{print $2}')
+INTERFACES=$(echo "$INTERFACE_NAMES" | grep -v "lo")
+MENU=()
+for interface in $INTERFACES
+do
+	MENU+=("$interface" "Available Network Interface")
+done
+INTERFACE=$(whiptail \
+--title "Network Interface" \
+--menu "Choose Network Interface" \
+15 60 5 \
+"${MENU[@]}" \
+3>&1 1>&2 2>&3)
 
 # IP
 IP=$(whiptail --inputbox "Please enter your static IP address" 10 40 3>&1 1>&2 2>&3)
+if [[ $? -ne 0 ]]; then
+    whiptail --msgbox "Configuration Cancelled" 10 40
+    exit 1
+fi
 
 # CIDR
 CIDR=$(whiptail --inputbox "Please enter CIDR" 10 40 3>&1 1>&2 2>&3)
+if [[ $? -ne 0 ]]; then
+    whiptail --msgbox "Configuration Cancelled" 10 40
+    exit 1
+fi
 
 # gateway
 GATEWAY=$(whiptail --inputbox "Please enter your gateway IP" 10 40 3>&1 1>&2 2>&3)
+if [[ $? -ne 0 ]]; then
+    whiptail --msgbox "Configuration Cancelled" 10 40
+    exit 1
+fi
 
 # DNS Server
 DNS=$(whiptail --inputbox "Please enter your DNS Server" 10 40 3>&1 1>&2 2>&3) 
+if [[ $? -ne 0 ]]; then
+    whiptail --msgbox "Configuration Cancelled" 10 40
+    exit 1
+fi
 
 # validating ips using a fucntion
 validateIP(){
 	local ip=$1
+	if [[ -z "$ip" ]];then
+		return 1
+	fi
+
 	if [[ !$ip =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]];then
 		return 1
 	fi
@@ -43,11 +76,16 @@ validateIP(){
 
 # validate inteface
 validateInterface(){
-	if ip link show "$1" > /dev/null 2>&1; then
-		return 0
-	else 
+	local interface=$1
+	if [[ -z "$interface" ]]; then
 		return 1
 	fi
+
+	if ! ip link show "$INTERFACE"; then
+		whiptail --msgbox "Invalid Interface chose" 10 40
+		return 1
+	fi
+
 }
 
 #validate CIDR
@@ -58,6 +96,12 @@ validateCIDR(){
 	       return 1
 	fi
 }
+
+# validate interface
+if ! validateInterface "$INTERFACE"; then
+       whiptail --msgbox "Invalid Interface name" 10 40
+       exit 1
+fi
 
 # validate ip
 if ! validateIP "$IP"; then
@@ -90,25 +134,32 @@ network:
   renderer: networkd
 
   ethernets:
-   $INTERFACE:
-    dhcp4: no
-    addresses: 
-      - $IP/$CIDR
+    $INTERFACE:
+      dhcp4: no
+    
+      addresses: 
+        - $IP/$CIDR
 
-    routes:
-      - to: default
-        via: $GATEWAY
+      routes:
+        - to: default
+          via: $GATEWAY
 
-    nameservers:
-     addresses:
-       - $DNS
+      nameservers:
+        addresses:
+          - $DNS
 EOF
 
 # Applying netplan configuration
-netplan apply
+netplan try
 
-#success message
-whiptail --msgbox "Successfully configured" 10 52
+if [[ $? -ne 0 ]]; then
+	whiptail --msgbox "Netplan configuration failed" 10 40
+	exit 1
+fi
+
+# success message
+whiptail --msgbox "SUccessfully configured" 10 40
+
 
 
 
