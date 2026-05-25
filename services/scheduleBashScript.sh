85717015fb8f3309f090cba796d50900a98e6ae0
nandiniiee <jain.nandini.squash@gmail.com>
2026-05-25 10:07:50 +0000

    added bashscript and scheduled using cron also

--- services/scheduleBashScript.sh
+++ services/scheduleBashScript.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+BINARY="/home/nandini/bashScripting/services/copyBinary"
+SOURCE="/home/nandini/bashScripting/services/file1.txt"
+DESTINATION="/home/nandini/bashScripting/services/copied.txt"
 
+# passing them as arguments
+$BINARY $SOURCE $DESTINATION
+
+# we will do scheduling through cron
