7723c5a5654b5b2a649a6e359575c71f1ac45b0e
nandiniiee <jain.nandini.squash@gmail.com>
2026-05-25 09:10:02 +0000

    c program to copy the files added

--- services/copyProgram.c
+++ services/copyProgram.c
@@ -1,7 +1,40 @@
 #include<stdio.h>
-int main(){
-	int x=10;
-	printf("value of x is ");
-	printf("%d\n", x);
+#include<stdlib.h>
+int main(int argc, char *argv[]){
+	FILE *sourcefile;
+	FILE *destinationfile;
+	// argc=3 (source, destination and argv[0] will be executable
+	if(argc!=3){
+		printf("source and destination file, both not mentioned");
+		exit(1);
+	}
+
+	//open source file
+	sourcefile=fopen(argv[1], "r");
+	if(sourcefile==NULL){
+		printf("Error in opening the file\n");
+		exit(1);
+	}
+	
+	//open destination file
+	destinationfile=fopen(argv[2],"w");
+	if(destinationfile==NULL){
+		printf("Error opening the destination file\n");
+		fclose(sourcefile);
+		exit(1);
+	}
+
+	//copying contents from source to destination
+	int ch;
+	while((ch=fgetc(sourcefile)) !=EOF){
+		fputc(ch, destinationfile);
+	}
+	printf("File copying completed successfully\n");
+	fclose(sourcefile);
+	fclose(destinationfile);
+	return 0;
+
+
 }
+
 
--- services/scheduleBashScript.sh
+++ services/scheduleBashScript.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "hie this works"
+
